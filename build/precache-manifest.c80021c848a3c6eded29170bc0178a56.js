94168ce99cca415588505959ae8fd5f1f32baf75
mathangspk <mathangspk@gmail.com>
2023-06-03 17:29:59 +0700

    update

---
+++ build/precache-manifest.c80021c848a3c6eded29170bc0178a56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1856c5113a5d9a39a4b2f72f9fcefae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb705ff493f381038e18",
+    "url": "/static/css/2.c8ade6f6.chunk.css"
+  },
+  {
+    "revision": "bb705ff493f381038e18",
+    "url": "/static/js/2.8fed3d31.chunk.js"
+  },
+  {
+    "revision": "74d9b785c63f1b68dc7e7bd22513a2a1",
+    "url": "/static/js/2.8fed3d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f95027a11fa708a76ba",
+    "url": "/static/js/main.f42ebd42.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f42ebd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38b390bfe7033db6033",
+    "url": "/static/js/runtime-main.4f38e04e.js"
+  },
+  {
+    "revision": "c4c1f06492ead5c53bba21d5414672fa",
+    "url": "/static/media/loading.c4c1f064.gif"
+  }
+]);
